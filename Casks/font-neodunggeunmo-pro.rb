c19a847dfaf54dfd7458ab2be6994b1e4af0367a
Dalgona <me@dalgona.dev>
2022-05-26 00:28:05 +0900

    Fix URL for font-neodunggeunmo-pro

--- Casks/font-neodunggeunmo-pro.rb
+++ Casks/font-neodunggeunmo-pro.rb
@@ -3,11 +3,11 @@ cask "font-neodunggeunmo-pro" do
   sha256 "e4b974b1a378ef1962e3c72dfbcb7b7bba09f54dd53bf57889af300b2531b155"
 
   # github.com/neodgm/neodgm-pro/ was verified as official when first introduced to the cask
-  url "https://github.com/neodgm/neodgm-pro/releases/download/v#{version}/neodgm_pro.ttf",
+  url "https://github.com/neodgm/neodgm-pro/releases/download/v#{version}/NeoDunggeunmoPro-Regular.ttf",
       verified: "github.com/neodgm/neodgm-pro/"
   name "NeoDunggeunmo Pro"
   desc "Proportional font based on NeoDunggeunmo"
   homepage "https://neodgm.dalgona.dev/neodgm_pro.html"
 
-  font "neodgm_pro.ttf"
+  font "NeoDunggeunmoPro-Regular.ttf"
 end
