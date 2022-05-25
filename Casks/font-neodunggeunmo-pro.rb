20f4c15fdc44b4f0545c86450867af73e0209753
Dalgona <me@dalgona.dev>
2022-05-25 18:54:20 +0900

    Update neodgm-pro to v1.020

--- Casks/font-neodunggeunmo-pro.rb
+++ Casks/font-neodunggeunmo-pro.rb
@@ -1,6 +1,6 @@
 cask "font-neodunggeunmo-pro" do
-  version "1.011"
-  sha256 "290d9ac6288c729fac80d9af00837eeeaa51f9516610e737980e8711096a0446"
+  version "1.020"
+  sha256 "e4b974b1a378ef1962e3c72dfbcb7b7bba09f54dd53bf57889af300b2531b155"
 
   # github.com/neodgm/neodgm-pro/ was verified as official when first introduced to the cask
   url "https://github.com/neodgm/neodgm-pro/releases/download/v#{version}/neodgm_pro.ttf",
