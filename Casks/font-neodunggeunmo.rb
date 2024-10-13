d42f8faae77270e694e2b065e1f5fd530ad1b02b
Dalgona. <me@dalgona.dev>
2024-10-13 10:17:59 +0900

    Fix incorrect version in font-neodunggeunmo cask

--- Casks/font-neodunggeunmo.rb
+++ Casks/font-neodunggeunmo.rb
@@ -1,5 +1,5 @@
 cask "font-neodunggeunmo" do
-  version "1.530"
+  version "1.600"
   sha256 "d61b60eccb731f8ca9c7da582e4a05a94db66b570471809950aa9a7261b941d6"
 
   # github.com/neodgm/neodgm/ was verified as official when first introduced to the cask
