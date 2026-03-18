72a9de28c0cfff777acd4a997d9bae3c0095ec8e
Dalgona. <me@dalgona.dev>
2026-03-18 23:39:22 +0900

    Update neodgm and neodgm-code to v1.601

--- Casks/font-neodunggeunmo-code.rb
+++ Casks/font-neodunggeunmo-code.rb
@@ -1,6 +1,6 @@
 cask "font-neodunggeunmo-code" do
-  version "1.600"
-  sha256 "3d58bf707f06bcebefb57355197efe31a89cd20d48a9cb5bb6e2e4758d830b23"
+  version "1.601"
+  sha256 "0d19cda34cba2c68137d4328820b743141279f8533b5624521f7b325e270a265"
 
   # github.com/neodgm/neodgm/ was verified as official when first introduced to the cask
   url "https://github.com/neodgm/neodgm/releases/download/v#{version}/neodgm_code.ttf",
--- Casks/font-neodunggeunmo.rb
+++ Casks/font-neodunggeunmo.rb
@@ -1,12 +1,12 @@
 cask "font-neodunggeunmo" do
-  version "1.600"
-  sha256 "d61b60eccb731f8ca9c7da582e4a05a94db66b570471809950aa9a7261b941d6"
+  version "1.601"
+  sha256 "77305267996073aae07bad9313dad2e306a4128e55bfafbed4c41558fee57b4d"
 
   # github.com/neodgm/neodgm/ was verified as official when first introduced to the cask
   url "https://github.com/neodgm/neodgm/releases/download/v#{version}/neodgm.ttf",
       verified: "github.com/neodgm/neodgm/"
   name "NeoDunggeunmo"
-  desc "TrueType font based on an old-but-good Korean bitmap font"
+  desc "Modern TrueType font based on a timeless Korean bitmap font"
   homepage "https://neodgm.dalgona.dev/"
 
   font "neodgm.ttf"
